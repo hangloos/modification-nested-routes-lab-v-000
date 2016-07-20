8cf1435671df27e7aae3c1f7907edd6c866ca4db
hangloos <ericloos00@gmail.com>
2016-07-20 14:52:53 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -7,6 +7,7 @@ module SongsHelper
         select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
       else
         hidden_field_tag "song[artist_id]", song.artist_id
+        "#{artist.name}: "
       end
     else
     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
